ccbfb7786c38bb4f9e43e3b27205cf2b6b24ef5c
Marius Costin Dracea <mariusdracea@gmail.com>
2018-10-24 17:58:09 +0300

    EOD commit

--- battle_details.py
+++ battle_details.py
@@ -1,5 +1,7 @@
 import random
+
 from tank_specifications import *
+
 
 # This will select which tank fires first
 def coin_flip():
@@ -10,6 +12,7 @@ def coin_flip():
     else:
         first_to_shoot = "T34"
     return  first_to_shoot
+
 
 def shot(receiver, damage):
     receiver = receiver - damage
@@ -23,19 +26,55 @@ def target_choice(armor):
         return target_armor
 
 
-def fire(target, damage):
-    target -= damage
-    return  target
-
-
-def hit_chance():
+def hit_chance(author, receiver):
     percentage_chance = random.randint(0,100)
-    print("You have" + " " + str(percentage_chance) + "% chances to get it right.")
+    print(author+ " " + "has" + " " + str(percentage_chance) + "% chances to hit" + " " + receiver)
     if random.randint(0,100) < percentage_chance:
         return True
     else:
         return False
 
 
+def round():
 
-round()
+    if coin_flip() == "Panzer":
+        author = "Panzer"
+        receiver = 'T34'
+        if hit_chance(author, receiver) is True:
+            try:
+                item_hit_1st = target_choice(t34_defensive_stats)
+                item_hit_1st -= panzer_damage[0]
+                print("T34 tank was hit")
+            except Exception as e:
+                print('T34 tank has been defeated')
+        else:
+            print("MISS")
+        author = "T34"
+        receiver = 'Panzer'
+        if hit_chance(author, receiver) is True:
+            item_hit_2nd = target_choice(panzer_defensive_stats)
+            item_hit_2nd -= t34_damage[0]
+            print("Panzer tank was hit")
+        else:
+            print("Miss")
+    elif coin_flip() == "T34:":
+        author = "T34"
+        receiver = 'Panzer'
+        if hit_chance(author, receiver) is True:
+            item_hit_1st = target_choice(panzer_defensive_stats)
+            item_hit_1st -= t34_damage[0]
+            print("Panzer tank was hit")
+        else:
+            print("MISS")
+        author = "Panzer"
+        receiver = 'T34'
+        if hit_chance(author, receiver) is True:
+            item_hit_2nd = target_choice(t34_defensive_stats)
+            item_hit_2nd -= panzer_damage[0]
+            print("T34 tank was hit")
+        else:
+            print("Miss")
+
+
+
+
--- tank_specifications.py
+++ tank_specifications.py
@@ -3,7 +3,7 @@ import mysql.connector
 mysql_db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password = "R@hl8047",
+    password = "password",
     database="tanks_db"
 )
 
